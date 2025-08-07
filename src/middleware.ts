24836be7881e411358efbcf56ae00f38223907d7
Strife-Cyber <dunamisjunior15@gmail.com>
2025-08-07 21:51:09 +0100

    webhooks work configure well in stack auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,13 @@ import { stackServerApp } from "@/stack";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  // Skip /api/webhook and anything under it
+  if (pathname.startsWith('/api/webhooks')) {
+    return NextResponse.next(); // skip middleware
+  }
+
   const user = await stackServerApp.getUser();
   if (!user) {
     return NextResponse.redirect(new URL('/handler/sign-in', request.url));
